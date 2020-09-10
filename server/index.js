bcab18b91fcb87500011e8509772eb545b8be417
Muhanned Noman <Muhanned.Noman@gmail.com>
2020-09-10 18:37:28 +0300

    Feat(index): Allow graphiql to test in browser.

--- server/index.js
+++ server/index.js
@@ -4,7 +4,7 @@ import { PORT } from './env';
 import schema from './schema/schema';
 
 const app = express();
-app.use('/graphql', graphqlHTTP({ schema }));
+app.use('/graphql', graphqlHTTP({ schema, graphiql: true }));
 
 const port = 4000 || PORT;
 app.listen(port, () => {
