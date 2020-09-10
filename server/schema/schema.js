3380395cce0bf6fcf66cab8f20907f5973d338af
Muhanned Noman <Muhanned.Noman@gmail.com>
2020-09-10 18:36:22 +0300

    Feat(schema): Add some mock data for testing.

--- server/schema/schema.js
+++ server/schema/schema.js
@@ -1,4 +1,12 @@
 import { GraphQLObjectType, GraphQLSchema, GraphQLString } from 'graphql';
+import _ from 'lodash';
+
+// dumb data
+const books = [
+  { name: 'Name of the wind', genre: 'Fantasy', id: '1' },
+  { name: 'The final Empire', genre: 'Fantasy', id: '2' },
+  { name: 'The Long Earth', genre: 'Sci-Fi', id: '3' },
+];
 
 const BookType = new GraphQLObjectType({
   name: 'Book',
@@ -19,6 +27,7 @@ const RootQuery = new GraphQLObjectType({
       },
       resolve(parent, args) {
         //TODO get data from db
+        return _.find(books, { id: args.id });
       },
     },
   },
