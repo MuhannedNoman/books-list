105ea19c477012f18bfdbf01b4fe421b1ad8f847
Muhanned Noman <Muhanned.Noman@gmail.com>
2020-09-10 20:35:53 +0300

    Feat(schema): add Author root query type.

--- server/schema/schema.js
+++ server/schema/schema.js
@@ -1,5 +1,6 @@
 import {
   GraphQLID,
+  GraphQLInt,
   GraphQLObjectType,
   GraphQLSchema,
   GraphQLString,
@@ -13,12 +14,27 @@ const books = [
   { name: 'The Long Earth', genre: 'Sci-Fi', id: '3' },
 ];
 
+const authors = [
+  { name: 'Patrick Rothfuss', age: 44, id: '1' },
+  { name: 'Brandon Sanderson', age: 42, id: '2' },
+  { name: 'Terry Pratchett', age: 66, id: '3' },
+];
+
 const BookType = new GraphQLObjectType({
   name: 'Book',
   fields: () => ({
     id: { type: GraphQLID },
     name: { type: GraphQLString },
     genre: { type: GraphQLString },
+  }),
+});
+
+const AuthorType = new GraphQLObjectType({
+  name: 'Author',
+  fields: () => ({
+    id: { type: GraphQLID },
+    name: { type: GraphQLString },
+    age: { type: GraphQLInt },
   }),
 });
 
@@ -33,6 +49,16 @@ const RootQuery = new GraphQLObjectType({
       resolve(parent, args) {
         //TODO get data from db
         return _.find(books, { id: args.id });
+      },
+    },
+    author: {
+      type: AuthorType,
+      args: {
+        id: { type: GraphQLID },
+      },
+      resolve(parent, args) {
+        //TODO get data from db
+        return _.find(authors, { id: args.id });
       },
     },
   },
