1b9290e50bf8559473cf92b36f55965c9f9aa216
Muhanned Noman <Muhanned.Noman@gmail.com>
2020-09-10 21:45:41 +0300

    Chore(server):connect local mongoose db to project

--- server/index.js
+++ server/index.js
@@ -1,7 +1,15 @@
 import express from 'express';
 import { graphqlHTTP } from 'express-graphql';
-import { PORT } from './env';
+import mongoose from 'mongoose';
+import { DB_CONNECTION, PORT } from './env';
 import schema from './schema/schema';
+
+mongoose
+  .connect(DB_CONNECTION, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  })
+  .then(console.log('Conncted to the database'));
 
 const app = express();
 app.use('/graphql', graphqlHTTP({ schema, graphiql: true }));
