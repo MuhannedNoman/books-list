d3b87c1ec01b8f560fb289a8cfa5e248f988e6ac
Muhanned Noman <Muhanned.Noman@gmail.com>
2020-09-10 21:23:16 +0300

    Feat(schema): link the list of books to author

--- server/schema/schema.js
+++ server/schema/schema.js
@@ -1,6 +1,7 @@
 import {
   GraphQLID,
   GraphQLInt,
+  GraphQLList,
   GraphQLObjectType,
   GraphQLSchema,
   GraphQLString,
@@ -9,9 +10,12 @@ import _ from 'lodash';
 
 // dumb data
 const books = [
-  { name: 'Name of the wind', genre: 'Fantasy', id: '1', authorId: '1' },
-  { name: 'The final Empire', genre: 'Fantasy', id: '2', authorId: '2' },
+  { name: 'Name of The Wind', genre: 'Fantasy', id: '1', authorId: '1' },
+  { name: 'The Final Empire', genre: 'Fantasy', id: '2', authorId: '2' },
   { name: 'The Long Earth', genre: 'Sci-Fi', id: '3', authorId: '3' },
+  { name: 'The Hero of Ages', genre: 'Fantasy', id: '4', authorId: '2' },
+  { name: 'The Colour of Magic', genre: 'Fantasy', id: '5', authorId: '3' },
+  { name: 'The Light Fantastic', genre: 'Fantasy', id: '6', authorId: '3' },
 ];
 
 const authors = [
@@ -41,6 +45,12 @@ const AuthorType = new GraphQLObjectType({
     id: { type: GraphQLID },
     name: { type: GraphQLString },
     age: { type: GraphQLInt },
+    books: {
+      type: new GraphQLList(BookType),
+      resolve(parent, args) {
+        return _.filter(books, { authorId: parent.id });
+      },
+    },
   }),
 });
 
