106156624f4fa7de6597cd405643c90af1d4d0b0
Muhanned Noman <Muhanned.Noman@gmail.com>
2020-09-12 07:02:34 +0300

    Chore(server): add cors dependency
    this allow querying data from a client app.

--- server/index.js
+++ server/index.js
@@ -1,3 +1,4 @@
+import cors from 'cors';
 import express from 'express';
 import { graphqlHTTP } from 'express-graphql';
 import mongoose from 'mongoose';
@@ -12,6 +13,7 @@ mongoose
   .then(console.log('Conncted to the database'));
 
 const app = express();
+app.use(cors());
 app.use('/graphql', graphqlHTTP({ schema, graphiql: true }));
 
 const port = 4000 || PORT;
