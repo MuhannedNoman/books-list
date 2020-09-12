019286f64483cdd44040b4782e413635290042ed
Muhanned Noman <Muhanned.Noman@gmail.com>
2020-09-12 07:43:42 +0300

    Feat(BookList): get data from graphql
    get books list from GraphQl and display them to screen.

--- src/components/BookList/BookList.jsx
+++ src/components/BookList/BookList.jsx
@@ -1,11 +1,28 @@
+import { gql, useQuery } from '@apollo/client';
 import React from 'react';
 
+const GET_BOOKS = gql`
+  {
+    books {
+      name
+      id
+    }
+  }
+`;
+
 const BookList = () => {
+  const { loading, error, data } = useQuery(GET_BOOKS);
+
+  if (loading) return <p>Loading books...</p>;
+  if (error) return <p>Error :(</p>;
+
+  const books = data.books.map((book) => {
+    return <li key={book.id}>{book.name}</li>;
+  });
+
   return (
     <div>
-      <ul id="book-list">
-        <li>Book name</li>
-      </ul>
+      <ul id="book-list">{books}</ul>
     </div>
   );
 };
