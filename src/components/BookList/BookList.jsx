c34fa14300b8b1c8633280339713c0970494be47
Muhanned Noman <Muhanned.Noman@gmail.com>
2020-09-12 06:24:15 +0300

    Feat(BookList): add component

---
+++ src/components/BookList/BookList.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+const BookList = () => {
+  return (
+    <div>
+      <ul id="book-list">
+        <li>Book name</li>
+      </ul>
+    </div>
+  );
+};
+
+export default BookList;
