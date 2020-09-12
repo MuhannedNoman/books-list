69f5e879ebfbcae20cee12973f1221cee2bfb0c9
Muhanned Noman <Muhanned.Noman@gmail.com>
2020-09-12 06:48:10 +0300

    Chore: correct apollo dependency

--- src/App.js
+++ src/App.js
@@ -1,12 +1,21 @@
+import { ApolloClient, ApolloProvider, InMemoryCache } from '@apollo/client';
 import React from 'react';
 import BookList from './components/BookList';
+import { CLIENT_LINK } from './env';
+
+const client = new ApolloClient({
+  uri: CLIENT_LINK,
+  cache: new InMemoryCache(),
+});
 
 const App = () => {
   return (
-    <div>
-      <h1>Reading List</h1>
-      <BookList />
-    </div>
+    <ApolloProvider client={client}>
+      <div>
+        <h1>Reading List</h1>
+        <BookList />
+      </div>
+    </ApolloProvider>
   );
 };
 
