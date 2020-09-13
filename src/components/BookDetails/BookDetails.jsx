ae48af2dd404295dc44234f561fb4d23613de53f
Muhanned Noman <Muhanned.Noman@gmail.com>
2020-09-13 06:27:25 +0300

    Feat(BookDetails): display selected book detail
    this will display information about the book author and
    their other work.

--- src/components/BookDetails/BookDetails.jsx
+++ src/components/BookDetails/BookDetails.jsx
@@ -2,17 +2,34 @@ import { useQuery } from '@apollo/client';
 import React from 'react';
 import { GET_BOOK } from '../../queries/Queries';
 
-const BookDetails = () => {
-  const { loading, error, data } = useQuery(GET_BOOK);
+const BookDetails = ({ id }) => {
+  const { loading, error, data } = useQuery(GET_BOOK, { variables: { id } });
 
   if (loading) return <p>Loading books...</p>;
   if (error) return <p>Error :(</p>;
 
-  return (
-    <div id="book-details">
-      <p>Detail here</p>
-    </div>
-  );
+  const bookDetail = () => {
+    const { book } = data;
+    if (book) {
+      return (
+        <div>
+          <h2>{book.name}</h2>
+          <p>{book.genre}</p>
+          <p>{book.author.name}</p>
+          <p>All books by this author:</p>
+          <ul className="other-books">
+            {book.author.books.map((book) => (
+              <li key={book.id}>{book.name}</li>
+            ))}
+          </ul>
+        </div>
+      );
+    } else {
+      return <div>No book selected...</div>;
+    }
+  };
+
+  return <div id="book-details">{bookDetail()}</div>;
 };
 
 export default BookDetails;
--- src/components/BookList/BookList.jsx
+++ src/components/BookList/BookList.jsx
@@ -1,22 +1,33 @@
 import { useQuery } from '@apollo/client';
-import React from 'react';
+import React, { useState } from 'react';
 import { GET_BOOKS } from '../../queries/Queries';
 import BookDetails from '../BookDetails';
 
 const BookList = () => {
+  const [bookId, setBookId] = useState(null);
+
   const { loading, error, data } = useQuery(GET_BOOKS);
 
   if (loading) return <p>Loading books...</p>;
   if (error) return <p>Error :(</p>;
 
   const books = data.books.map((book) => {
-    return <li key={book.id}>{book.name}</li>;
+    return (
+      <li
+        onClick={() => {
+          setBookId(book.id);
+        }}
+        key={book.id}
+      >
+        {book.name}
+      </li>
+    );
   });
 
   return (
     <div>
       <ul id="book-list">{books}</ul>
-      <BookDetails />
+      <BookDetails id={bookId} />
     </div>
   );
 };
